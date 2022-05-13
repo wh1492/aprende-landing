9fdd03fc92dcf547e91cb399c66901172c3a94c0
Leonelo Acaban <leoneloacaban@gmail.com>
2022-05-12 21:50:47 -0300

    fix filter

--- src/components/courses/index.js
+++ src/components/courses/index.js
@@ -4,10 +4,9 @@ import Loader from '../commons/loader'
 import './courses.scss'
 
 
-const CardCourse = ({ course }) => {
-  // const id = course.url.split('/').filter(x => x).pop()
+const CardCourse = ({ course, category, actCat }) => {
   return (
-    <div className="card-course">
+    <div className={actCat == category || actCat == 'all' ? 'card-course show' : "card-course hidden"} >
       <picture className="card-course--thumb">
         <source srcSet={course.meta.thumbnail.sizes.desktop} media="(min-width: 600px)" />
         <img src={course.meta.thumbnail.sizes.tablet} alt={course.slug} />
@@ -22,7 +21,14 @@ const CardCourse = ({ course }) => {
 const Courses = () => {
   const [loading, setLoading] = useState(true)
   const [posts, setPosts] = useState({});
+  const [postsSec, setPostsSec] = useState({});
   const [postCount, setPostCount] = useState(4)
+  const [moreCount, setMoreCount] = useState(0)
+  const [postCat, setPostCat] = useState('all')
+
+  const filterPost = posts
+  const allPosts = postsSec
+  let filtrado = []
 
   function viewMore() {
     setPostCount(postCount + 4)
@@ -38,14 +44,36 @@ const Courses = () => {
     );
     const data = await response.json();
     setPosts(data);
+    setPostsSec(data);
     setLoading(false)
+    setPostCount(4)
+    setMoreCount(data.length)
   };
 
   useEffect(() => {
     fetchPost();
+    setPostCat('all')
   }, []);
 
+  function filterPosts(catSlug) {
+    setPostCat(catSlug)
+    setPostCount(4)
+    setLoading(true)
+    setTimeout(() => setLoading(false), 500)
+    allPosts.filter(post => post.taxonomy['master-class-category'][0]).
+      map(filteredPost => {
+        // 
+        if (filteredPost.taxonomy['master-class-category'][0].slug == catSlug) {
+          filtrado.push(filteredPost)
+          setPosts(filtrado);
+          setMoreCount(filtrado.length)
+        } else if (catSlug == 'all') {
+          setPosts(postsSec)
+          setMoreCount(postsSec.length)
+        }
+      })
 
+  }
 
   return (
     <div className="aprende-courses">
@@ -53,10 +81,11 @@ const Courses = () => {
 
         <h4>Explora todas nuestras Clases Magistrales</h4>
 
+
         <ul className="aprende-courses--categories">
-          <li><button id="all" className="active">Todas</button></li>
-          <li><button id="chocolateria">Chocolatería</button></li>
-          <li><button id="manicure">Manicure</button></li>
+          <li><button onClick={() => filterPosts('all')} id="all" className={postCat == 'all' ? 'active' : ''}>Todas</button></li>
+          <li><button onClick={() => filterPosts('chocolateria')} id="chocolateria" className={postCat == 'chocolateria' ? 'active' : ''}>Chocolatería</button></li>
+          <li><button onClick={() => filterPosts('manicure')} id="manicure" className={postCat == 'manicure' ? 'active' : ''}>Manicure</button></li>
         </ul>
 
         {loading && (
@@ -65,17 +94,19 @@ const Courses = () => {
 
         {!loading && (
           <>
-
             <div className="aprende-courses--list">
               {
-                posts.slice(0, postCount).map(post => <CardCourse course={post} key={post.id} />)
+                filterPost.slice(0, postCount).map(post => <CardCourse course={post} actCat={postCat} category={post.taxonomy['master-class-category'][0].slug} key={post.id} />)
               }
             </div>
-            <button className={loading ? 'btn-more active' : 'btn-more'} onClick={() => viewMore()}>Ver más</button>
+            {
+              moreCount > 4 && (
+                <button className={loading ? 'btn-more active' : 'btn-more'} onClick={() => viewMore()}>Ver más</button>
+              )
+            }
           </>
         )
         }
-
 
       </div>
     </div>
